6ddfc9c90a815c8d3016ab6381865a85258e0984
vinnielopes <64753885+vinnielopes@users.noreply.github.com>
2021-09-03 10:54:45 -0400

    Completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,15 @@
+"""
+Name: Vinicius Nunes Lopes
+hello.py
+
+Problem: Test the environment with a hello function
+
+Certification of authenticity:
+I certify that this assignment is entirely my own work
+
+"""
+
+def main():
+    print("hello, world!")
+
+main()
