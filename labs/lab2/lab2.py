5e4085253cdeb5dfa8be0158e3eb0058f10fe0dc
vinnielopes <64753885+vinnielopes@users.noreply.github.com>
2021-09-06 15:46:57 -0400

    complete lab2

--- labs/lab2/lab2.py
+++ labs/lab2/lab2.py
@@ -7,18 +7,34 @@ I certify that this assignment is entirely my own work
 """
 import math
 
-def multiplication_table():
+def sum_of_three():
+    number = eval(input("Input an upper bound: "))
+    sum = 0
+    for x in range(0, number -1, 3):
+        a = 3 + x
+        sum = sum + a
 
-    for i in range (1,11):
-        print( 1 * i, 2 * i, 3 * i, 4 * i, 5 * i, 6 * i, 7 * i, 8 * i, 9 * i, 10 * i)
+    print(sum)
+
+sum_of_three()
+
+def multiplication_table():
+    y = eval(input("Choose a multiple: "))
+    y = y + 1
+    for row in range(1, y):
+        # print(i,":", 1 * i, 2 * i, 3 * i, 4 * i, 5 * i, 6 * i, 7 * i, 8 * i, 9 * i, 10 * i)
+        print(row, ":", end=" ")
+        for col in range(1,y):
+            print (row * col, end="\t")
+        print()
 
 multiplication_table()
 
 
 def triangle_area():
-    a = 6
-    b = 6
-    c = 6
+    a = eval(input("Input the length of side a: "))
+    b = eval(input("Input the length of side b: "))
+    c = eval(input("Input the length of side c: "))
     s = (a + b + c) / 2
     area = math.sqrt( s * (s-a) * (s-b) * (s-c))
     print(" area of the triangle: ", area)
@@ -28,16 +44,24 @@ triangle_area()
 def sum_of_squares():
     lower = eval(input("Input lower bound: "))
     upper = eval(input("Input upper bound: "))
+    sum = 0
     for i in range(0, (upper+1)-lower):
-        print(math.pow(lower+i,2))
+        square = math.pow(lower+i,2)
+        sum = sum + square
+
+    print(sum)
 
 sum_of_squares()
 
 def power():
     base = eval(input("Input the base: "))
     exponent = eval(input("input the exponent: "))
-    for i in range (exponent):
-        print(base ** i)
+    number = 1
+    for i in range(exponent):
+        number = number * base
+
+    print(base, "^", exponent, "=", number)
+
 power()
 
 
