d634e4ec3916ee7818cfd2813a5965e948aee990
vinnielopes <64753885+vinnielopes@users.noreply.github.com>
2021-10-25 15:08:45 -0400

    Completed lab8

---
+++ assignments/hw7/weighted_average.py
@@ -0,0 +1,49 @@
+"""
+Name: Vinicius Nunes Lopes
+weighted_average.py.py
+
+Problem: Calculate the weighted average from a text file
+
+Certification of authenticity:
+I certify that this assignment is entirely my own work
+"""
+
+def weighted_average(in_file_name, out_file_name):
+    grades = open(in_file_name, 'r')
+    count = len(open(in_file_name).readlines())
+
+    average = open(out_file_name, 'w')
+    error = 0
+    acc = 0
+    for lines in grades:
+        sum = 0
+        total_w = 0
+        name_grades = lines.strip("\n",).split(":")
+        name = name_grades[0]
+        grades = name_grades[1].strip().split()
+        for i in range(0, len(grades), 2):
+            number = eval(grades[i]) * eval(grades[i + 1])
+            sum = sum + number
+            total_w = total_w + eval(grades[i])
+
+        if total_w == 100:
+            weighted_average = sum/100
+        else:
+            weighted_average = 0
+        acc = (acc + weighted_average)
+
+        if total_w < 100:
+            average.write(name + "'s average: Error: The weights are less than 100.\n")
+            error = error + 1
+        if total_w > 100:
+            average.write(name + "'s average: Error: The weights are more than 100.\n")
+            error = error + 1
+        else:
+            average.write(name + "'s average: " + str(round(weighted_average, 1)) + "\n")
+    average.write("Class average: " + str(round((acc/(count-error)), 1)) + "\n")
+
+def main():
+    weighted_average("grades.txt", "avg.txt")
+
+if __name__ == '__main__':
+    main()
--- labs/lab8/lab8.py
+++ labs/lab8/lab8.py
@@ -1,10 +1,73 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Vinicius Nunes Lopes
+lab8.py
 """
 
+def number_words(in_name, out_name):
+    infile = open(in_name, "r")
+    outfile = open(out_name, "w+")
+    i = 1
+    for line in infile:
+        words = line.split()
+        for word in words:
+            outfile.write(str(i) + " " + word + "\n")
+            i = i + 1
+
+def hw(inname, outname):
+    infile = open(inname, "r")
+    outname = open(outname, "w+")
+
+    for line in infile:
+        puts = line.split()
+        wage = eval(puts[2]) + 1.65
+        total_wage = wage * eval(puts[3])
+        outname.write(puts[0]+ " " + puts[1] + " " + str(wage) + " " + str(total_wage) + "\n")
+
+def isbn(isbn_str):
+    acc = 0
+    for i in range(10):
+        position = 10 - i
+        acc = acc + (eval(isbn_str[i]) * position)
+    return acc
+
+def sm(file, friend):
+    infile = open(file, "r")
+    outfile = open(friend+".txt", "w+")
+    for line in infile:
+        outfile.write(line)
+
+def encode(message, integer_value):
+    acc = ''
+    for i in range(len(message)):
+        number = ord(message[i])
+        letter = chr(number + integer_value)
+        acc = acc + letter
+    return acc
+
+def ssn(file, friend, key):
+    infile = open(file, "r")
+    outfile = open(friend + ".txt", "w+")
+    for line in infile:
+        new = encode(line, key)
+        outfile.write(new)
+
+def ssu(file, friend, pad):
+    infile = open(file, "r")
+    outfile = open(friend + ".txt", "w+")
+    padfile = open(pad, "r")
+    key = padfile.read()
+    for line in infile:
+        new = encode_better(line, padfile)
+        outfile.write(new)
+
+
 def main():
-    # add other function calls here
+    number_words("Walrus.txt", "count.txt")
+    hw("hourly_wages.txt", "raised_hourly_wages")
+    isbn("0012345678")
+    sm("message.txt", "bob")
+    ssn("safest_message.txt", "bianca", 1)
+    ssu("safest_message.txt", "robert", "pad.txt")
     pass
 
 
